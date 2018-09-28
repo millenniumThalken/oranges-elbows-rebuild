2ef41e9a167149c06bdc0dae5d47b002900df4d7
millenniumThalken <patrick-thalken@uiowa.edu>
2018-09-28 14:14:55 -0500

    homepage is rendering and no warnings are flagged

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+    animations: [
+        trigger('slideInOut', [
+            state('in', style({
+                transform: 'translate3d(0,0,0)'
+            })),
+            state('out', style({
+                transform: 'translate3d(100%,0,0)'
+            })),
+            transition('in => out', animate('400ms ease-in-out')),
+            transition('out => in', animate('400ms ease-in-out'))
+        ]),
+    ]
+})
+export class HeaderComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+
+    menuState: string = 'out';
+
+    toggleMenu() {
+        this.menuState = this.menuState === 'out' ? 'in' : 'out';
+    }
+
+}
